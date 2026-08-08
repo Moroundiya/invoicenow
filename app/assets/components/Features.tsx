63eebed71bc0f26bade96e97d52cf3b92d77d7f5
Moroundiya <moroundiya@outlook.com>
2026-08-08 20:19:19 +0200

    Features section

--- app/assets/components/Features.tsx
+++ app/assets/components/Features.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import SharpFlashOnIcon from "@iconify-react/ic/sharp-flash-on";
+import TemplateFilledIcon from "@iconify-react/tabler/template-filled";
 
 export default function Features() {
 	return (
@@ -18,37 +21,39 @@ export default function Features() {
 				<div className="mt-12 grid grid-cols-3 gap-16">
 					<div className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
 						<div className="w-1/5 h-full flex justify-center">
-							<div className="w-12 h-12 rounded-full bg-[#00B7FF]"></div>
+							<div className="relative w-12 h-12">
+								<div className="absolute inset-0 translate-y-[1.5px] translate-x-[1.5px] rounded-full bg-[#00B7FF]" />
+								<div className="relative w-full h-full rounded-full bg-[#0066FF] flex justify-center items-center">
+									<SharpFlashOnIcon className="text-white h-8" />
+								</div>
+							</div>
 						</div>
 						<div className="w-4/5">
-							<p className="text-xl text-white">Create in Seconds</p>
+							<p className="text-xl text-white font-allucra font-regular">
+								Create in seconds
+							</p>
 							<p className="text-[#eeeeee9e] mt-2 w-10/12 leading-[1.1] font-light">
 								Create professional invoices in seconds with out easy-to-use
-								invoice builder
+								invoice builder.
 							</p>
 						</div>
 					</div>
 					<div className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
 						<div className="w-1/5 h-full flex justify-center">
-							<div className="w-12 h-12 rounded-full bg-[#00B7FF]"></div>
+							<div className="relative w-12 h-12">
+								<div className="absolute inset-0 translate-y-[1.5px] translate-x-[1.5px] rounded-full bg-[#00B7FF]" />
+								<div className="relative w-full h-full rounded-full bg-[#0066FF] flex justify-center items-center">
+									<TemplateFilledIcon className="text-white h-8" />
+								</div>
+							</div>
 						</div>
 						<div className="w-4/5">
-							<p className="text-xl text-white">Create in Seconds</p>
-							<p className="text-[#eeeeee9e] mt-2 w-10/12 leading-[1.1] font-light">
-								Create professional invoices in seconds with out easy-to-use
-								invoice builder
+							<p className="text-3xl text-white font-allura font-regular">
+								Custom Templates
 							</p>
-						</div>
-					</div>
-					<div className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
-						<div className="w-1/5 h-full flex justify-center">
-							<div className="w-12 h-12 rounded-full bg-[#00B7FF]"></div>
-						</div>
-						<div className="w-4/5">
-							<p className="text-xl text-white">Create in Seconds</p>
-							<p className="text-[#eeeeee9e] mt-2 w-10/12 leading-[1.1] font-light">
-								Create professional invoices in seconds with out easy-to-use
-								invoice builder
+							<p className="text-[#eeeeee9e] mt-0 w-10/12 leading-[1.1] font-light">
+								Choose from beautoful template and customize them to match your
+								taste.
 							</p>
 						</div>
 					</div>
