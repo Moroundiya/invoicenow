95de233e1cb2056d3b8cca9aa211e2b63789c004
Moroundiya <moroundiya@outlook.com>
2026-08-10 20:19:19 +0200

    Features section

--- app/assets/components/Features.tsx
+++ app/assets/components/Features.tsx
@@ -2,6 +2,8 @@
 
 import SharpFlashOnIcon from "@iconify-react/ic/sharp-flash-on";
 import TemplateFilledIcon from "@iconify-react/tabler/template-filled";
+import AllCurrencyIcon from "@iconify-react/fluent-mdl2/all-currency";
+import { features } from "@/data/featuresData";
 
 export default function Features() {
 	return (
@@ -18,63 +20,29 @@ export default function Features() {
 					Everything You Need to Get Paid Faster
 				</p>
 
-				<div className="mt-12 grid grid-cols-3 gap-16">
-					<div className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
-						<div className="w-1/5 h-full flex justify-center">
-							<div className="relative w-12 h-12">
-								<div className="absolute inset-0 translate-y-[1.5px] translate-x-[1.5px] rounded-full bg-[#00B7FF]" />
-								<div className="relative w-full h-full rounded-full bg-[#0066FF] flex justify-center items-center">
-									<SharpFlashOnIcon className="text-white h-8" />
+				<div className="mt-12 grid grid-cols-3 gap-6">
+					{features.map((feature) => (
+						<div
+							key={feature.title}
+							className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
+							<div className="w-1/5 h-full flex justify-center">
+								<div className="relative w-12 h-12">
+									<div className="absolute inset-0 translate-y-[1.5px] translate-x-[1.5px] rounded-full bg-[#00B7FF]" />
+									<div className="relative w-full h-full rounded-full bg-[#0066FF] flex justify-center items-center">
+										{feature.icon}
+									</div>
 								</div>
 							</div>
-						</div>
-						<div className="w-4/5">
-							<p className="text-3xl text-white font-allura font-regular">
-								Create in seconds
-							</p>
-							<p className="text-[#eeeeee9e] mt-0 w-10/12 leading-[1.1] font-light">
-								Create professional invoices in seconds with out easy-to-use
-								invoice builder.
-							</p>
-						</div>
-					</div>
-					<div className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
-						<div className="w-1/5 h-full flex justify-center">
-							<div className="relative w-12 h-12">
-								<div className="absolute inset-0 translate-y-[1.5px] translate-x-[1.5px] rounded-full bg-[#00B7FF]" />
-								<div className="relative w-full h-full rounded-full bg-[#0066FF] flex justify-center items-center">
-									<TemplateFilledIcon className="text-white h-8" />
-								</div>
+							<div className="w-4/5">
+								<p className="text-3xl text-white font-allura font-regular">
+									{feature.title}
+								</p>
+								<p className="text-[#eeeeee9e] mt-0 w-10/12 leading-[1.1] font-light">
+									{feature.description}
+								</p>
 							</div>
 						</div>
-						<div className="w-4/5">
-							<p className="text-3xl text-white font-allura font-regular">
-								Custom Templates
-							</p>
-							<p className="text-[#eeeeee9e] mt-0 w-10/12 leading-[1.1] font-light">
-								Choose from beautiful template and customize them to match your
-								taste.
-							</p>
-						</div>
-					</div>
-					<div className="w-full h-[120px] rounded-2xl bg-[#041f5049] border-2 border-[#041E50] p-3 flex space-x-3">
-						<div className="w-1/5 h-full flex justify-center">
-							<div className="relative w-12 h-12">
-								<div className="absolute inset-0 translate-y-[1.5px] translate-x-[1.5px] rounded-full bg-[#00B7FF]" />
-								<div className="relative w-full h-full rounded-full bg-[#0066FF] flex justify-center items-center">
-									<TemplateFilledIcon className="text-white h-8" />
-								</div>
-							</div>
-						</div>
-						<div className="w-4/5">
-							<p className="text-3xl text-white font-allura font-regular">
-								Multiple Currencies
-							</p>
-							<p className="text-[#eeeeee9e] mt-0 w-10/12 leading-[1.1] font-light">
-								Bill your international clients with support for multiple currencies
-							</p>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</div>
---
+++ data/featuresData.tsx
@@ -0,0 +1,30 @@
+import SharpFlashOnIcon from "@iconify-react/ic/sharp-flash-on";
+import TemplateFilledIcon from "@iconify-react/tabler/template-filled";
+import AllCurrencyIcon from "@iconify-react/fluent-mdl2/all-currency";
+
+export const features = [
+	{
+		icon: <SharpFlashOnIcon className="text-white h-8" />,
+		title: "Create in seconds",
+		description:
+			"Create professional invoices in seconds with our easy-to-use invoice builder.",
+	},
+	{
+		icon: <TemplateFilledIcon className="text-white h-8" />,
+		title: "Custom Templates",
+		description:
+			"Choose from beautiful templates and customize them to match your taste.",
+	},
+	{
+		icon: <AllCurrencyIcon className="text-white h-8" />,
+		title: "Multiple Currencies",
+		description:
+			"Bill your international clients with support for multiple currencies.",
+	},
+	{
+		icon: <AllCurrencyIcon className="text-white h-8" />,
+		title: "Download & Share",
+		description:
+			"Download as PDF/PNG or share directly with your clients via email.",
+	},
+];
