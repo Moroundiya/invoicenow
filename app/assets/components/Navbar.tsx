5509819c540f7272c6d2f6ab38e68de96f127b05
Moroundiya <moroundiya@outlook.com>
2026-08-12 08:45:48 +0100

    Navbar design

--- app/assets/components/Navbar.tsx
+++ app/assets/components/Navbar.tsx
@@ -15,30 +15,30 @@ export default function Navbar() {
 				<div className="hidden md:flex space-x-8 text-lg">
 					<Link
 						href="/"
-						className="group relative inline-block overflow-hidden px-2 py-2 font-medium text-[#0047D9] transition-colors duration-300 hover:text-white">
-						<span className="absolute inset-0 origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
-
-						<span className="relative z-10">Home</span>
-					</Link>
-					<Link
-						href="/"
-						className="group relative inline-block overflow-hidden rounded-lg px-4 py-2 font-medium text-[#0047D9] transition-colors duration-300 hover:text-white">
-						<span className="absolute inset-0 origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
-						<span className="relative z-10">Features</span>
+						className="group relative inline-block px-2 py-2 font-medium text-[#0047D9]">
+						Home
+						<span className="absolute bottom-0 left-0 h-[2px] w-full origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
 					</Link>
 
 					<Link
 						href="/"
-						className="group relative inline-block overflow-hidden rounded-lg px-4 py-2 font-medium text-[#0047D9] transition-colors duration-300 hover:text-white">
-						<span className="absolute inset-0 origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
-						<span className="relative z-10">How it works</span>
+						className="group relative inline-block px-4 py-2 font-medium text-[#0047D9]">
+						Features
+						<span className="absolute bottom-0 left-0 h-[2px] w-full origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
 					</Link>
 
 					<Link
 						href="/"
-						className="group relative inline-block overflow-hidden rounded-lg px-4 py-2 font-medium text-[#0047D9] transition-colors duration-300 hover:text-white">
-						<span className="absolute inset-0 origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
-						<span className="relative z-10">About</span>
+						className="group relative inline-block px-4 py-2 font-medium text-[#0047D9]">
+						How it works
+						<span className="absolute bottom-0 left-0 h-[2px] w-full origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
+					</Link>
+
+					<Link
+						href="/"
+						className="group relative inline-block px-4 py-2 font-medium text-[#0047D9]">
+						About
+						<span className="absolute bottom-0 left-0 h-[2px] w-full origin-left scale-x-0 bg-[#0047D9] transition-transform duration-300 ease-out group-hover:scale-x-100" />
 					</Link>
 				</div>
 				<Link
